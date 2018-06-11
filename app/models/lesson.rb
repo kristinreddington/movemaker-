d5ce94e30adf4d844e3928aa685c71c2c6ba016f
kristinreddington <kristinreddington@gmail.com>
2018-06-11 00:40:59 -0400

    first validations & querying methods

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,24 @@
 class Lesson < ApplicationRecord
-has_many :bookings
-has_many :users, :through => :bookings 
+  has_many :bookings
+  has_many :users, :through => :bookings
+
+  def display_date
+    display_day + " at" + display_time
+  end
+
+  def display_day
+    self.date.strftime("%B%e,%Y")
+  end
+
+  def display_time
+    self.time.strftime("%l:%M %P")
+  end
+
+  def expired_lesson?
+    expired_days = Lesson.where('date < ?', DateTime.now)
+    expired_times = Lesson.where('time < ?', Time.now)
+    expired_days.include?(self) || expired_times.include?(self)
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :bookings
-  has_many :lessons, :through => :bookings 
+  has_many :lessons, :through => :bookings
+
+  validates :name, :presence => true
+  #before_validation :strip_whitespace
+
+  #def strip_whitespace
+    #self.name.gsub!(/(\A\s*|\s*\z)/, '')
+  #end
 end
