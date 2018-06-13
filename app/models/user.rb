d35db8f9e055c1f3e968717de1a6367795716f3c
kristinreddington <kristinreddington@gmail.com>
2018-06-12 20:11:43 -0400

    added omniauth query method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,16 @@ class User < ApplicationRecord
   has_many :lessons, :through => :bookings
 
   validates :name, :presence => true
-  validates :name, :uniqueness => {message: -> (object, data) do
-    "Hey #{object.name}, #{data.value} has already been taken! Please try with another name."
-  end }
-  
+  validates :email, :presence => true
+  validates :email, :uniqueness => true
+
   auto_strip_attributes :name
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    where(:email => auth_hash['info']['email']).first_or_create do |user|
+      user.name = auth_hash['info']['name']
+      user.password = SecureRandom.hex
+    end
+  end
 
 end
