d5ff3bb34f129f4b5a3f50603eda1aa011220a66
kristinreddington <kristinreddington@gmail.com>
2018-06-19 22:39:11 -0400

    add show and edit methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,11 @@
 class BookingsController < ApplicationController
 
 
+  def show
+    @booking = Booking.find_by(:id => params[:id])
+    @user = current_user
+  end
+
   def create
       booking = Booking.create(
         :user_id => (params[:lesson][:user_id]),
@@ -9,12 +14,29 @@ class BookingsController < ApplicationController
     message = booking.book_lesson
    redirect_to user_path(booking.user, danger: message)
 
-end
+ end
+
+  def edit
+    @booking = Booking.find_by(:id => params[:id])
+  end
+
+  def update
+    @booking = Booking.find_by(:id => params[:id])
+    @booking.update(booking_params)
+    @booking.save
+    redirect_to user_path(current_user)
+  end
 
   def destroy
     @booking = Booking.find_by(:id => params[:id])
     current_user.bookings.delete(@booking)
     redirect_to user_path(current_user)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:comment)
   end
 
 end
