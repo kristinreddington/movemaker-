5bdefdf0f402e01502fc6770f97e7cdd8699e55d
kristinreddington <kristinreddington@gmail.com>
2018-06-20 09:30:57 -0400

    add expired lessons route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-
+  get '/lessons/expired' => 'lessons#expired', as: :expired_lessons
   resources :lessons
 
+
   resources :users do
-    post '/bookings' => 'bookings#create'
-    resources :bookings, only: [:destroy, :show]
+    resources :bookings, only: [:new, :create, :edit, :show, :update]
   end
+  get '/bookings/:id/delete' => 'bookings#destroy', as: :delete_booking
 
   get '/signin' => 'sessions#new'
-  resources :sessions, only: [:create]
-  post '/signout' => 'sessions#destroy'
+  resources :sessions, :only => [:create]
+  get '/logout' => 'sessions#destroy'
+
+  get '/auth/facebook/callback' => 'sessions#create'
 end
