32c1f957a985b4e9fba7e510a12a3b2be0405e88
kristinreddington <kristinreddington@gmail.com>
2018-07-13 21:34:30 -0400

    add lesson jquery

--- app/assets/javascripts/lessons.js
+++ app/assets/javascripts/lessons.js
@@ -1,1 +1,27 @@
+$(function () {
+  $(".js-more").on('click', function() {
+    var id = $(this).data("id");
+    $.get("/lessons/" + id + "/info" + ".json", function(data) {
+      let lessonInfo = new Lesson(data.genre, data.level, data.date, data.time)
+      let formattedLessonInfo = lessonInfo.formatLessonInfo();
+      console.log(data.genre)
+      $("#info-" + id).append(formattedLessonInfo);
+    });
+  });
 
+function Lesson(genre, level, date, time) {
+  this.genre = genre;
+  this.level = level;
+  this.date = date;
+  this.time = time;
+}
+
+Lesson.prototype.formatLessonInfo = function(){
+  let lessonHtml = '';
+  lessonHtml += `<li>${this.genre}</li>`
+  lessonHtml += `<li>${this.level}</li>`
+  lessonHtml += `<li>${this.date}</li>`
+  lessonHtml += `<li>${this.time}</li>`
+  return lessonHtml;
+  }
+});
