9e87ec412922aa6940623309dff43d67f70bb747
kristinreddington <kristinreddington@gmail.com>
2018-07-19 17:34:39 -0400

    add destroy booking resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
+
   get '/lessons/expired' => 'lessons#expired', as: :expired_lessons
   resources :lessons
-
+  get 'lessons/:id/info' => 'lessons#info', as: :lessons_info
 
   resources :users do
-    resources :bookings, only: [:new, :create, :edit, :show, :update]
+    resources :bookings, only: [:new, :create, :edit, :show, :update, :destroy] do
+      resources :notes, only: [:index, :create, :destroy]
+    end
   end
-  get '/bookings/:id/delete' => 'bookings#destroy', as: :delete_booking
+
+  #get '/bookings/:id/delete' => 'bookings#destroy', as: :delete_booking
 
   get '/signin' => 'sessions#new'
   resources :sessions, :only => [:create]
